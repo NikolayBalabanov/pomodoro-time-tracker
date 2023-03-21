183df6ef95fcf733329f2d9f6c8b8c67bc274264
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-03-21 09:44:19 +0300

    initial

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+    coverage: {
+      provider: 'c8',
+      exclude: [
+        'vite-env.d.ts',
+        'src/main.tsx',
+        'postcss.config.cjs',
+        'tailwind.config.cjs',
+        'vite.config.ts',
+      ],
+      skipFull: false,
+      reporter: 'text',
+    },
+  },
+});
