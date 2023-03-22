839045cf5a45c0e620bed0e1b7d1fa7c455318e7
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-03-22 20:00:48 +0300

    feat: add tasks and dropdown

--- src/components/DescriptionItem.tsx
+++ src/components/DescriptionItem.tsx
@@ -5,5 +5,5 @@ interface IDescriptionItemProps {
 }
 
 export default function DescriptionItem({ text }: IDescriptionItemProps) {
-  return <li className="list-item text-base leading-8 pl-[23px]">{text}</li>;
+  return <li className="descr-item text-base leading-8 pl-[23px]">{text}</li>;
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import TaskItem from './TaskItem';
 
 export default function TaskList() {
-  return <div>TaskList</div>;
+  return (
+    <div>
+      <ul className="mb-5 w-[370px] flex flex-col items-start list-none">
+        <TaskItem count={1} title="Сверстать сайт" id={0} />
+        <TaskItem count={2} title="Проверить валидность" id={1} />
+        <TaskItem count={1} title="Купить хлеба" id={2} />
+      </ul>
+      <span>1 час 15 мин</span>
+    </div>
+  );
 }
