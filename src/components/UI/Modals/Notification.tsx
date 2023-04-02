f4017d24ed7a1126baf3b9c13d9472ee59617611
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-04-02 19:05:45 +0300

    feat: fix confirm and add notification modal

--- src/components/UI/Modals/Notification.tsx
+++ src/components/UI/Modals/Notification.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import ButtonClose from '../ButtonClose';
 import ButtonConfirm from '../ButtonConfirm';
-import ButtonUnderscored from '../ButtonUnderscored';
+import ButtonClose from '../ButtonClose';
 
 interface IConfirmProps {
   onClose: () => void;
-  onConfirm: () => void;
 }
 
-export default function Confirm({ onClose, onConfirm }: IConfirmProps) {
+export default function Notification({ onClose }: IConfirmProps) {
   const modal = useRef<HTMLDivElement | null>(null);
   const modalContent = useRef<HTMLDivElement | null>(null);
 
@@ -39,11 +37,12 @@ export default function Confirm({ onClose, onConfirm }: IConfirmProps) {
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex flex-col-reverse text-center mb-[20px] justify-between items-center">
-          <h2 className="text-2xl font-medium leading-[17px] text-gray-900">Удалить задачу?</h2>
+          <h2 className="text-2xl font-medium leading-[17px] text-gray-900">
+            Чтобы изменить задачу, остановите таймер
+          </h2>
         </div>
-        <div className="flex flex-col">
-          <ButtonConfirm text="Удалить" callback={() => close(onConfirm)} />
-          <ButtonUnderscored text="Отмена" callback={() => close(onClose)} />
+        <div className="flex flex-col items-center">
+          <ButtonConfirm text="Понятно" callback={() => close(onClose)} />
         </div>
         <ButtonClose callback={() => close(onClose)} />
       </div>
