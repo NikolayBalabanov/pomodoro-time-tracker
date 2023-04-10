11a254df2018f840b69013ba7a2d9b3c026bfa15
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-04-10 08:49:56 +0300

    feat: add extra minute logic

--- src/components/TimerLayout/TimerDisplay.tsx
+++ src/components/TimerLayout/TimerDisplay.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { EStages } from '../../models/timer';
+import { useAppDispatch } from '../../hooks/redux';
+import { addExtraMinute } from '../../redux/Slices/timerSlice';
 
 interface ITmerDisplay {
   time: string;
@@ -8,6 +10,10 @@ interface ITmerDisplay {
 }
 
 export default function TimerDisplay({ stage, time, isRunning }: ITmerDisplay) {
+  const dispatch = useAppDispatch();
+  const handleAddExtraMinute = () => {
+    dispatch(addExtraMinute());
+  };
   const curColor = !isRunning
     ? 'text-colorText dark-mode dark:text-white'
     : stage === EStages.session
@@ -18,7 +24,10 @@ export default function TimerDisplay({ stage, time, isRunning }: ITmerDisplay) {
       <div className={`font-extralight md:text-[150px] text-[90px] leading-none ${curColor}`}>
         {time}
       </div>
-      <button className="absolute top-1/2 left-full -translate-y-1/2 md:translate-x-[28px] translate-x-[2px] btn-pluse"></button>
+      <button
+        onClick={() => handleAddExtraMinute()}
+        className="absolute top-1/2 left-full -translate-y-1/2 md:translate-x-[28px] translate-x-[2px] btn-pluse"
+      ></button>
     </div>
   );
 }
