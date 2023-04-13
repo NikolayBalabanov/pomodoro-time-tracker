ba9b664e6b7bb85656de88a278a2f8afc62caead
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-04-13 12:48:37 +0300

    fix: total time calc

--- src/utils/getTotalTaskTime.ts
+++ src/utils/getTotalTaskTime.ts
@@ -1,5 +1,5 @@
 import { ITask } from 'models/task';
 
 export const getTotalTasksTime = (tasksArr: ITask[], session: number) => {
-  return tasksArr.reduce((acc, cur) => acc + cur.rounds * session, 0);
+  return tasksArr.reduce((acc, cur) => acc + cur.rounds * session * 60, 0);
 };
